84b3c59909183a665c6488cda157f401fd1211f9
Ivo Castillo <saavedraivo@hotmail.com>
2020-05-08 10:13:08 +0200

    making the first function to work with the data

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -1,21 +1,39 @@
+// to use cin & cout mostly as I understand it. 
 #include <iostream>
+/* to use ifstream & ofstream, we used ofstream to store a grid on another file. 
+if the file does not exist it makes one.
+we now use the ifstream to stream the file content, which we will use to make our grid.
+*/
 #include <fstream>
+// pretty self-explanatory
 #include <vector>
+//we use sstream to split up the txt file.
+#include <sstream>
 
+//Making a function to read the grid and print it out.
+void ReadGrid (std::string path){
+    std::ifstream sgrid(path);
+    std::string line;
 
-int main() {
-
-    std::vector<int> gridRow {0,0,0,0,0,0,1};
-    //making a txt file called grid where I will save the grid
-    //manually added the rest of the grid out of laziness
-    std::ofstream stream("grid.txt");
-
-    //pushing the vector row into the txt file.
-
-    for (auto v: gridRow) {
-        stream << v << ',';
+    while (getline(sgrid,line)){
+        std::cout<< line << "\n";
     }
+}
 
-//this code will all be deleted on the next commit except the grid.txt file
-return 0;
+//Now I want to not just show the info, but save it in a 2d vector to work with the data.
+// So first I will make a function to get a row from the grid, and save it in a 1d vector.
+std::vector<int> GridRow (std::string line) {
+    std::vector<int> row;
+    int n;
+    char c;
+    std::istringstream sline(line);
+    while (sline >> n >> c && c == ',') {
+        row.push_back(n);
+    }
+    return row;
+}
+
+
+int main() {   
+    ReadGrid("grid.txt");
 }
