e23c91335c2ebc101cbaf798bfd0d9674cc8005f
Ivo Castillo <saavedraivo@hotmail.com>
2020-05-08 10:39:08 +0200

    Adjusting the functions to call on each other. and store the data from the grid.txt

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -10,18 +10,10 @@ we now use the ifstream to stream the file content, which we will use to make ou
 //we use sstream to split up the txt file.
 #include <sstream>
 
-//Making a function to read the grid and print it out.
-void ReadGrid (std::string path){
-    std::ifstream sgrid(path);
-    std::string line;
 
-    while (getline(sgrid,line)){
-        std::cout<< line << "\n";
-    }
-}
 
 //Now I want to not just show the info, but save it in a 2d vector to work with the data.
-// So first I will make a function to get a row from the grid, and save it in a 1d vector.
+// This function is used to get a one line of strings, and split it into a number, the whole row will go into a 1d vector.
 std::vector<int> GridRow (std::string line) {
     std::vector<int> row;
     int n;
@@ -33,6 +25,21 @@ std::vector<int> GridRow (std::string line) {
     return row;
 }
 
+// now i'm changing this to return a 2d vector.
+// Also I had to change the position of the functions. Since this function calls on GridRow,
+// the function needs to be defined first to be able to get called upon.
+std::vector<std::vector<int>> ReadGrid (std::string path){
+    std::vector<std::vector<int>> board;
+    std::ifstream sgrid(path);
+    std::string line;
+
+    while (getline(sgrid,line)){
+        //making a 1d vector that will be take each string row, and push every vector row into the 2d vector.
+        std::vector<int> row = GridRow(line);
+        board.push_back(row);
+    }
+    return board;
+}
 
 int main() {   
     ReadGrid("grid.txt");
